a00845732001a107eefa894e8420f177cc1287a7
JohannesPoulsen <92087736+JohannesPoulsen@users.noreply.github.com>
2022-06-09 14:17:36 +0200

    Login screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'Master.dart';
 
+FocusNode myFocusNode = FocusNode();
 void main() => runApp(MaterialApp(
       home: Home(),
     ));
@@ -13,7 +14,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
-      home: HomeScreen(),
+      home: Login(),
     );
   }
 }
@@ -89,7 +90,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           FloatingActionButton(
             onPressed: () {
-              Navigator.push(
+              Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(builder: (context) => const CreateWorkout()),
               );
@@ -142,7 +143,10 @@ class CreateWorkout extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            );
           },
           child: const Text('Go back!'),
         ),
@@ -151,3 +155,130 @@ class CreateWorkout extends StatelessWidget {
   }
 }
 
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'FitnessBuddy',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: nameController,
+                  focusNode: myFocusNode,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    labelText: 'User Name',
+                    labelStyle: TextStyle(
+                        color:
+                            myFocusNode.hasFocus ? Colors.blue : Colors.black),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(width: 0.5, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(width: 3, color: Colors.black),
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                        color:
+                            myFocusNode.hasFocus ? Colors.blue : Colors.black),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 0.5,
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(15)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(width: 3, color: Colors.black),
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  //forgot password screen
+                },
+                style: TextButton.styleFrom(primary: Colors.black),
+                child: const Text(
+                  'Forgot Password',
+                ),
+              ),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Login'),
+                    style: ElevatedButton.styleFrom(primary: Colors.black),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
+                      );
+                    },
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('Does not have account?'),
+                  TextButton(
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                    onPressed: () {
+                      //Signup skal komme her
+                    },
+                  )
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
