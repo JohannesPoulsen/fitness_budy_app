8dcde2c2b0a0ea19cf1a6658e9beaa08628f016f
JohannesPoulsen <92087736+JohannesPoulsen@users.noreply.github.com>
2022-06-14 22:55:34 +0200

    Reorderable list

--- lib/Model/Rutine.dart
+++ lib/Model/Rutine.dart
@@ -1,11 +1,11 @@
-class Rutine{
+class Rutine {
   String name;
   bool public = false;
   String? url;
 
-  Rutine({required this.name, required this.public, this.url });
+  Rutine({required this.name, required this.public, this.url});
 
-  Rutine newRutine(String name, bool public){
+  Rutine newRutine(String name, bool public) {
     return Rutine(name: name, public: public);
   }
 }
