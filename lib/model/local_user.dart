cc6301bc6593bc7ca93b7acd47956dd1e65b2bcf
tailsdk <emil.s.o.dissing@gmail.com>
2022-06-21 09:44:55 +0200

    refactor

--- lib/model/local_user.dart
+++ lib/model/local_user.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:fitness_body_app/model/workout.dart';
 import 'package:fitness_body_app/main.dart';
 
